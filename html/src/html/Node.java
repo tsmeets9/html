af8a91adc9bff43698ce76c683f3e315e1dded13
tsmeets9 <47253276+tsmeets9@users.noreply.github.com>
2020-03-15 20:36:59 +0100

    Array implementation for children

--- html/src/html/Node.java
+++ html/src/html/Node.java
@@ -1,33 +1,68 @@
 package html;
 
+import java.util.Arrays;
+
 /**
+ * DIT ZIJN INVARIANTEN DIE DE GEBRUIKER KAN ZIEN, die de client kan zien
  * Version with only FirstChild implementation (not LastChild)
  * @author thiba
+ * @invar | (getTag() == null) == (getText() != null 
+ * @invar | getParent() == null || Arrays.stream(getParents().getChildren()).anyMatch(c -> c == this)
+ * @invar | Arrays.stream(getChildren()).anyMatch(c -> c!= null && c.getParent() == this)
  *
  */
 public class Node {
+	
+	// INTERNE DOCUMENTATIE, PER METHODE KAN JE CHECKEN OF DIT ZO IS. ALS ZE IN HET BEGIN VAN EEN METHODE GELDEN, GELDEN ZE DAN OOK OP HET EINDE
+	
+		/* ALtijd goed definiëren wat de geldige toestanden zijn
+		 * @invar Either {@code tag} or {@code text} must be non-null
+		 * 		|tag != null || text != null
+		 * Either {@code tag} or {@code text} must be null
+		 * 		|tag == null || text == null
+		 * 
+		 * zelfde als vorige twee beknopter geschreven:
+		 * @invar | (tag == null) == (text != null)
+		 * @invar |children != null    //kan je kiezen of je dit wilt, we moeten kiezen hoe we dan een lege lijst kinderen maken 
+		 * @invar |parent == null 
+		 * head is kind van html dus met html ook parent zijn van head
+		 * 
+		 * @invar If this node has a parent, this node is among it's parent's children.
+		 * 		  | parent == null || Arrays.stream(parents.children).anyMatch(c -> c == this)
+		 * @invar For each child of this node, the child's parent equals this node.
+		 * 		  | Arrays.stream(children).allMatch(c -> c != null && c.parent == this)
+		 * 
+		 */
 	private String tag;
 	private String text;
 	private Node parent;
-	private Node firstChild;
-	private Node lastChild;
-	private Node nextSibling;
-	private Node previousSibling;
+	private Node[] children;
+	
+	public String getTag() {return tag; }
+	public String getText() {return text;}
+	
+	public Node getParent() {return parent; }
+	// peer object 
+	
+	public Node[] getChildren() { 
+		// return children; //FOUT: REPRESENTATION EXPOSURE kan dan bv nulpointers erin steken, willen we vermijden. 
+		// we willen dat one code goed werkt zelfs als de klantcode niet tegoei werkt, interne objecten niet doorgeven aan de klant
+		// representation object
+		return Arrays.copyOf(children, children.length);
+	}
+	
 	
 	public Node(String tag, String text) {
 		this.tag = tag;
 		this.text = text;
+		this.children = new Node[0];
 }
+	
 	public void addChild(Node child) {
-		if (firstChild != null) {
-			lastChild.nextSibling = child;
-			child.previousSibling = lastChild;
-			lastChild = child;
-		}
-		else {
-			firstChild = child;
-			lastChild = child;
-		}
+		Node[] newChildren = new Node[children.length + 1];
+		System.arraycopy(children, 0, newChildren, 0, children.length);
+		newChildren[children.length] = child;
+		children = newChildren;
 		child.parent = this;
 	}
 	
@@ -55,20 +90,13 @@ public class Node {
 //	}
 	
 	public void removeChild(Node child) {
-		if (child == firstChild) { 
-			firstChild = firstChild.nextSibling;
-			if (firstChild == null)
-				lastChild = null;
-		} else {
-			if (child.nextSibling == null) {
-				lastChild = child.previousSibling;
-			} else {
-				child.nextSibling.previousSibling = child.previousSibling;
-			}
-			child.previousSibling.nextSibling = child.nextSibling;
-		}
-		child.nextSibling = null;
-		child.previousSibling = null;
+		Node[] newChildren = new Node[children.length -1];
+		int index = 0;
+		while (children[index] != child)
+			index++;
+		System.arraycopy(children, 0, newChildren, 0, index);
+		System.arraycopy(children, index + 1, newChildren, index, children.length - index - 1);
+		children = newChildren;
 		child.parent = null;
 	}
 	
@@ -78,11 +106,10 @@ public class Node {
 		if (text != null)
 			return text;
 		String result = "<" + tag + ">";
-		Node child = firstChild;
-		while (child != null) {
+		for (Node child: children)
+		// for (int i = 0; i < children.length; i++) {
+			// Node child = children[i]
 			result += child.toString();
-			child = child.nextSibling;
-		}
 		result += "</" + tag + ">";
 		return result;
 	}
